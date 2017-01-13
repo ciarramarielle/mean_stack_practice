afef4c93fb04c7d0c73c03421acd95a9824f86b9
ciarramarielle <ciarraaltoveros@gmail.com>
2017-01-13 01:08:10 +0000

    Basic node app setup on server.js

--- server.js
+++ server.js
@@ -10,3 +10,19 @@ var app = express();
 // express configuration
 // Express VIEWS property, set to path where to house views.
 app.set("views", __dirname + "/server/views");
+
+// view engine
+app.set("view engine", "jade");
+
+// route that delivers my index page
+// app.get("/"); //at route of website
+app.get("*", function(req, res) {
+    res.render("index"); // index within /server/views/
+}); // deliver index page at any request... for now
+
+// start listening to requests
+var port = 8080;
+app.listen(port);
+
+// check that server is running
+console.log("Listening on port " + port);
